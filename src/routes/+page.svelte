3d4684ebae5414132e6482a995ea49970dfb6b3d
Ronni Skansing <rskansing@gmail.com>
2023-10-10 15:59:52 +0200

    updated deps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script>
+	import { x } from '$lib/index.js';
+</script>
+
 <h1>Welcome to Sveltes it</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
